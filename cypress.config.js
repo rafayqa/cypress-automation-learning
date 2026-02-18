e523e7164ee59856ede317e22c451cc819e69561
rafayqa <m.rafay@venre.org>
2026-02-18 14:00:39 +0500

    push

--- cypress.config.js
+++ cypress.config.js
@@ -5,11 +5,62 @@ module.exports = defineConfig({
   env: {
     url: "https://rahulshettyacademy.com",
   },
+  retries: {
+    runMode: 1,
+  },
+  projectId: "r3x3eq",
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
-    specPattern : 'cypress/integration/examples/*.js'
+    specPattern: 'cypress/integration/examples/*.js'
   },
 });
+
+/*
+//const { defineConfig } = require("cypress");
+const browserify = require("@cypress/browserify-preprocessor");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  preprendTransformerToOptions,
+} = require("@badeball/cypress-cucumber-preprocessor/browserify");
+ 
+ async function setupNodeEvents(on,config)
+ {
+  await addCucumberPreprocessorPlugin(on, config);
+ 
+  on(
+    "file:preprocessor",
+    browserify(preprendTransformerToOptions(config, browserify.defaultOptions)),
+  );
+ 
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+ }
+ 
+ 
+module.exports = defineConfig({
+  defaultCommandTimeout: 6000,
+ 
+  env: {
+    url: "https://rahulshettyacademy.com",
+  },
+  reporter: 'cypress-mochawesome-reporter',
+ 
+  retries: {
+    runMode: 1,
+ 
+  },
+  projectId: "nodpcq",
+ 
+ 
+  e2e: {
+  setupNodeEvents,
+    specPattern: 'cypress/integration/examples/*.js'
+ 
+  },
+  */
--- cypress/integration/examples/Test9.js
+++ cypress/integration/examples/Test9.js
@@ -8,22 +8,10 @@ describe("Handling iframes in cypress", function () {
 
     cy.frameLoaded("#courses-iframe");
 
-    // Step 1: Click Mentorship
-    cy.iframe("#courses-iframe")
-      .find(".main-menu .navbar-collapse ul li a").contains("Mentorship")
-      .click();
+    cy.iframe("#courses-iframe").find("a[href*='mentorship']").eq(0).click();
 
-    // Step 2: Wait for iframe to reload
-    cy.frameLoaded("#courses-iframe");
-
-    // Step 3: Re-query iframe AFTER navigation
-    cy.iframe("#courses-iframe")
-      .find('.pricing-title', { timeout: 20000 })
-      .should('have.length', 2)
-      .each(($el, index) => {
-        cy.log(`Plan ${index + 1}: ${$el.text()}`);
-      });
+    cy.iframe("#courses-iframe").find("h1[class*='pricing-title']").should("have.length", 2);
 
   });
 
-});
+})
